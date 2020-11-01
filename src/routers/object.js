6e3c2f3cdd0602352fa2109938653bf80ab6e90a
PeterGuo <guojianglin123@gmail.com>
2020-11-01 19:50:43 +0800

    config router for deploying

--- src/routers/object.js
+++ src/routers/object.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = new express.Router()
 const objectController = require('../controllers/object')
 
-router.get('/', (req, res) => {
+router.get('/vault-test/', (req, res) => {
     res.send('hi, this is a default response to call in browser')
 })
 
-router.post('/object', objectController.saveObject)
-router.get('/object/:key', objectController.getObject)
+router.post('/vault-test/object', objectController.saveObject)
+router.get('/vault-test/object/:key', objectController.getObject)
 
 
 module.exports = router
--- test/object.test.js
+++ test/object.test.js
@@ -19,36 +19,36 @@ const object3 = {
     await new ObjectModel(object).save()
  })
 test('Testing post object endPoint.', async () => {
-    await request(app).post('/object').send(object).expect(201)
+    await request(app).post('/vault-test/object').send(object).expect(201)
 })
 
 test('Testing get object value by key name.', async () => {
-    await request(app).get(`/object/${object.key}`)
+    await request(app).get(`/vault-test/object/${object.key}`)
     .expect(200)
     .expect({value: object.value})
 })
 
 test('Testing get lastest object value by key name.', async () => {
-    await request(app).post('/object').send(object2)
-    await request(app).get(`/object/${object.key}`)
+    await request(app).post('/vault-test/object').send(object2)
+    await request(app).get(`/vault-test/object/${object.key}`)
     .expect(200)
     .expect({value: Object.values(object2)[0]})
 })
 
 test('Testing get object value by key name while filtering by timestamp', async () => {
-    const response2 = await request(app).post('/object').send(object2)
+    const response2 = await request(app).post('/vault-test/object').send(object2)
     const timestamp2 = response2.body['timestamp'] + 2
     console.log('timestamp2 === ', timestamp2)
     console.log('response2 body === ', response2.body)
 
-    const response3 = await request(app).post('/object').send(object3)
+    const response3 = await request(app).post('/vault-test/object').send(object3)
     const timestamp3 = response3.body['timestamp'] + 2
-    await request(app).get(`/object/${object.key}?timestamp=${timestamp2}`)
+    await request(app).get(`/vault-test/object/${object.key}?timestamp=${timestamp2}`)
     .expect(200)
     .expect({
         value: response2.body.value
     })
-    await request(app).get(`/object/${object.key}?timestamp=${timestamp3}`)
+    await request(app).get(`/vault-test/object/${object.key}?timestamp=${timestamp3}`)
     .expect(200)
     .expect({
         value: response3.body.value
